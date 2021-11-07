3ed15b57afc9f70cf68185e3b4ef76faaa439660
Dharanivarma9 <91324762+Dharanivarma9@users.noreply.github.com>
2021-11-07 15:59:43 +0530

    Update provider.tf

--- crometerraform/provider.tf
+++ crometerraform/provider.tf
@@ -15,6 +15,7 @@ provider "azurerm" {
    subscription_id="c5a3c50f-40e0-43ad-8722-e39ac7f364e7"
      tenant_id="dca0965c-9bd4-425f-8e9c-cf91dbc83a39"
      client_id="38602ed4-5891-4435-aa92-c904ff8f6970"
+     client_secret="ab8b03b0-413b-4712-ad0e-d829103b0fe2"
   features {
   }
 }
